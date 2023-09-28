57da916e352704a3bea8eb67128077e1a5f43e0e
DeveshPatodkar <21ec01031@iitbbs.ac.in>
2023-09-28 07:10:12 +0530

    added axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     // baseURL: "http://localhost:5000",
-    baseURL: "https://chatappbackend-kihr.onrender.com",
-    withCredentials: true,
+    baseURL: "https://chatappmern-production.up.railway.app",
+    // withCredentials: true,
 });
